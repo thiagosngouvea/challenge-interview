d0d291713be7fce577bbbf2ffc9674c6524055b4
thiagosngouvea <thiagosngouvea@gmail.com>
2024-11-22 19:24:01 -0300

    fix: :bug: ajustes no layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -5,18 +5,18 @@ import { useRouter } from "next/router";
 const { Header, Content, Footer } = Layout;
 
 const MENU_ITEMS = [
-    {
-      key: "1",
-      label: "Inicio",
-    },
-    {
-        key: "2",
-        label: "Entrevista",
-    },
-    {
-        key: "3",
-        label: "Respostas",
-    },
+  {
+    key: "/",
+    label: "Inicio",
+  },
+  {
+    key: "/interview",
+    label: "Entrevista",
+  },
+  {
+    key: "/answers",
+    label: "Respostas",
+  },
 ];
 
 interface LayoutProps {
@@ -26,26 +26,30 @@ interface LayoutProps {
 export default function LayoutSidebar({ children }: LayoutProps) {
   const router = useRouter();
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          {MENU_ITEMS.map(({ key, label }) => (
-            <Menu.Item 
-              key={key}
-              onClick={() => router.push(key === "1" ? "/" : key === "2" ? "/interview" : "/answers")}
-            >
-              {label}
-            </Menu.Item>
-          ))}
-        </Menu>
-      </Header>
-      <Content style={{ padding: "0 50px" }}>
-        <div className="site-layout-content">{children}</div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Desenvolvido por <a href="https://www.linkedin.com/in/thiago-gouv%C3%AAa-aa3bb915a/" target="_blank" rel="noreferrer">Thiago Gouvêa</a>
-      </Footer>
-    </Layout>
+  <Layout style={{ minHeight: "100vh" }}>
+    <Header>
+    <div className="logo" />
+    <Menu 
+      theme="dark" 
+      mode="horizontal" 
+      selectedKeys={[router.pathname]}
+    >
+      {MENU_ITEMS.map(({ key, label }) => (
+      <Menu.Item 
+        key={key}
+        onClick={() => router.push(key)}
+      >
+        {label}
+      </Menu.Item>
+      ))}
+    </Menu>
+    </Header>
+    <Content style={{ padding: "0 50px" }}>
+    <div className="site-layout-content">{children}</div>
+    </Content>
+    <Footer style={{ textAlign: "center" }}>
+      Desenvolvido por <a href="https://www.linkedin.com/in/thiago-gouv%C3%AAa-aa3bb915a/" target="_blank" rel="noreferrer">Thiago Gouvêa</a>
+    </Footer>
+  </Layout>
   );
 }
